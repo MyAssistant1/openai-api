78b8a640240707f03d17e80a019ecac37fd6b926
salih <salihtangel@gmail.com>
2023-11-25 16:24:51 +0300

    degisim

--- app.py
+++ app.py
@@ -10,6 +10,29 @@ from flask import Flask, request
 from pydub import AudioSegment
 from io import BytesIO
 import time
+import os
+
+def change_file_extension(file_path, new_extension):
+    # Dosya adını ve uzantısını ayrıştır
+    file_name, _ = os.path.splitext(file_path)
+
+    # Yeni uzantıyı ekleyerek yeni dosya adını oluştur
+    new_file_path = file_name + '.' + new_extension
+
+    # Dosyayı yeniden adlandır
+    os.rename(file_path, new_file_path)
+
+
+def delete_file(file_path):
+    try:
+        # Dosyayı sil
+        os.remove(file_path)
+        print(f"{file_path} başarıyla silindi.")
+    except FileNotFoundError:
+        print(f"{file_path} bulunamadı. Silme işlemi yapılamadı.")
+    except Exception as e:
+        print(f"Hata oluştu: {e}")
+
 
 app = Flask(__name__)
 CORS(app)
@@ -22,27 +45,16 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
 
-
-
-    import os
-
-    def change_file_extension(file_path, new_extension):
-        # Dosya adını ve uzantısını ayrıştır
-        file_name, _ = os.path.splitext(file_path)
-
-        # Yeni uzantıyı ekleyerek yeni dosya adını oluştur
-        new_file_path = file_name + '.' + new_extension
-
-        # Dosyayı yeniden adlandır
-        os.rename(file_path, new_file_path)
-
-    # Örnek kullanım
+   
+    # dosya ismi degistirme
     file_path = "/home/tangel/recorded_audio.ogx"
     new_extension = "mp3"
 
     change_file_extension(file_path, new_extension)
 
 
+    
+    #dosya okuma openai
     new_file = open("/home/tangel/recorded_audio.mp3", "rb")
     transcript = client.audio.translations.create(
     model="whisper-1", 
@@ -50,6 +62,10 @@ def upload():
     response_format="text"
 )
     print(transcript)
+
+    # dosyayi silme
+    file_to_delete = "/home/tangel/recorded_audio.mp3"  # Silmek istediğiniz dosyanın yolu ve adı
+    delete_file(file_to_delete)
     return 'Başarıyla alındı!'
 
 if __name__ == '__main__':
